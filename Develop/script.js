54a33e9c45ffae350fc75f4543d70ed559eea04e
Andrew <pelfreya6@gmail.com>
2024-03-20 18:16:40 -0400

    added my finishes to the starter code

---
+++ Develop/script.js
@@ -0,0 +1,123 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+
+const collectEmployees = function() {
+    const employees = [];
+    let addEmployees = true; 
+  // TODO: Get user input to create and return an array of employee objects
+
+  while (addEmployees) {
+    const firstName = prompt("Enter the employees first name: ");
+    const lastName = prompt("Enter the employees last name: ");
+    let salary = prompt("Enter the employees salary: ");
+    
+
+    if (isNaN(salary)) {
+        salary = 0;
+    }
+
+    employees.salary = salary.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+        
+
+    employees.push({
+        firstName: firstName,
+        lastName: lastName,
+        salary: salary
+    });
+
+    const addAnother = confirm("Do you want to add another employee?"); 
+    if (!addAnother) {
+        break;
+    }
+}
+  return employees
+}
+
+// Display the average salary
+const displayAverageSalary = function(employeesArray) {
+  // TODO: Calculate and display the average salary
+  let totalSalary = 0;
+  for (let i = 0; i < employeesArray.length; i++) {
+    totalSalary += employeesArray[i].salary;
+  }
+  const averageSalary = totalSalary / employeesArray.length;
+
+  console.log(`The average salary between our ${employeesArray.length} employee(s) is  ${averageSalary.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`);
+ }
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+  // TODO: Select and display a random employee
+   const luckyWinner = Math.floor(Math.random() * employeesArray.length);
+   const randomEmployee = employeesArray[luckyWinner];
+
+   console.log(`Congratulations ${randomEmployee.firstName} ${randomEmployee.lastName}, you are the winner!`);
+}
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # challenge-3
+
+I completed this assignment as a part of my bootcamp challenge!
+
+## Description
+
+In this challenge, I was provided the starter code but had to a few things to make it work! I had to create a function that displayed a series of prompts for the user to input their employees. Once the user finished inputting an employee, it gives them an toption to enter another one of cancel. This is the collectEmployees function. Once the user inputs the employees, I made an array of all the inputted employees, and wrote a function that calculated the average salary from the employees that the user put into the array. I also wrote a function that selected a random employee from the array with a math.random that selected a random winner!
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+My motivation behind making this project was to further develop my JavaScript and problem solving skills! I feel like this challenge was hard and the fact I got through it made me see I have what it takes to learn. I also learned how to properly google and search for things that can help me throughout my career. I learned a lot about making functions and arrays. Overall, I think this challenge was very useful in developing my Javascript, problem-solving, and googling skills!
+
+## Credits
+The Bootcamp instructors and team for providing the starter code and equipping me with tools to help complete!
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+
+
+
